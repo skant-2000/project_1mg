b5508ce0b6b37c1b3a62c1ee530fd614dfbc1f54
skant-2000 <qwertyskant2000@gmail.com>
2022-03-27 20:31:09 +0530

    advertisment carousels

---
+++ app/src/component/landingPage/HealthChcekupCarousel.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import style from "../../style/HealthCheckupCarousel.module.css"
+
+
+export default function HealthChcekupCarousel() {
+  return (
+    <div className={style.HealthChcekupCarousel}>
+        <div>
+            <p>Full body health checkups</p>
+            <button>SEE ALL</button>
+        </div>
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
