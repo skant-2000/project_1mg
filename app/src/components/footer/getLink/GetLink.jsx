a229d1cd0bf3e316776d44a4cb50ad6c33c2d5d9
skant-2000 <qwertyskant2000@gmail.com>
2022-04-02 02:06:12 +0530

    landing page footer css fix

--- app/src/components/footer/getLink/GetLink.jsx
+++ app/src/components/footer/getLink/GetLink.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import styles from "./getLink.module.css"
+import React from "react";
+import styles from "./getLink.module.css";
 export const GetLink = () => {
   return (
-		<div className={styles.forBoxShadow}>
-          <div className={styles.text}>Get the link to download App</div>
-          <div>
-              <input className={styles.inputLink} type="text" placeholder="Enter Phone Number" />
-              <button className={styles.buttonLink}>Send Link</button>
-          </div>
-		</div>
+    <div className={styles.forBoxShadow}>
+      <div className={styles.text}>Get the link to download App</div>
+      <div>
+        <input
+          className={styles.inputLink}
+          type="text"
+          placeholder="Enter Phone Number"
+        />
+        <button className={styles.buttonLink}>Send Link</button>
+      </div>
+    </div>
   );
-}
+};
